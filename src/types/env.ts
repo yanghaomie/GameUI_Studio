a3d8c234cd7255a4af8208861e590d78d9a0440c
yanghaomie <129841246+yanghaomie@users.noreply.github.com>
2026-03-23 18:19:52 +0800

    Add Firebase initialization scaffolding

---
+++ src/types/env.ts
@@ -0,0 +1,16 @@
+export type FirebaseClientEnv = {
+  FIREBASE_API_KEY: string;
+  FIREBASE_AUTH_DOMAIN: string;
+  FIREBASE_PROJECT_ID: string;
+  FIREBASE_STORAGE_BUCKET: string;
+  FIREBASE_MESSAGING_SENDER_ID: string;
+  FIREBASE_APP_ID: string;
+  FIREBASE_MEASUREMENT_ID?: string;
+};
+
+export type FirebaseAdminEnv = {
+  FIREBASE_ADMIN_PROJECT_ID: string;
+  FIREBASE_ADMIN_CLIENT_EMAIL: string;
+  FIREBASE_ADMIN_PRIVATE_KEY: string;
+  FIREBASE_ADMIN_STORAGE_BUCKET?: string;
+};
